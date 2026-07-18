53be5e0da5863a653d14858d37699f2f29cb6002
Vladislav <Vbabangida@bk.ru>
2026-07-18 23:33:00 +0300

    add fallback variables in axios base instance

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://rest.coincap.io/v3";
+
+const API_KEY = import.meta.env.VITE_API_KEY || "";
+
 export const coincapClient = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 coincapClient.interceptors.request.use((request) => {
-  request.headers["Authorization"] = `Bearer ${import.meta.env.VITE_API_KEY}`;
+  request.headers["Authorization"] = `Bearer ${API_KEY}`;
 
   return request;
 });
